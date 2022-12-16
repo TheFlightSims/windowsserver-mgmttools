7d733e79c1533fd83d8d58af8ff00a49ca1d535c
TheFlightSimsOfficial <theflightsimulations@gmail.com>
2022-12-16 08:19:21 +0700

    Delete code.py

--- db/getkeysonline/code.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import csv
-
-for i in range (1, 13):   
-    pre = str("Keys") + str(i) + str(".txt")
-    after = str("Keys") + str(i) + str(".csv")
-    pd.read_fwf(pre).to_csv(after)
-    
